19a6faf69a4b02f199a12279acf778dec9c1aeae
Punnamanikumar <punnamanikumar@gmail.com>
2022-03-10 13:24:44 +0530

    Creating Node and Express Server

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+const http = require("http");
 const express = require("express");
 const app = express();
 
+//! Creating NodeServer.
+http
+  .createServer((req, res) => {
+    res.write("Node Server");
+    res.end();
+  })
+  .listen(4004);
+
+//! Creating ExpressServer.
 app.get("/api", (req, res) => {
   const user = { name: "ManiKumar" };
   res.send(user);
